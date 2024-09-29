908c202b7855d10647908e7178da0134f221aaac
anhvi02 <anhvi09042002@gmail.com>
2024-09-29 22:46:53 +1000

    generate db

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
+# generate database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# route for index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+# run
 if __name__== "__main__":
     app.run(debug=True)
